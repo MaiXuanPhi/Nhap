a270bbcddefadfdecfe62f2b060374ec40a4fbe8
MaiXuanPhi <88835769+MaiXuanPhi@users.noreply.github.com>
2021-11-11 10:11:44 +0700

    hello3

--- app/src/main/java/com/example/nhap/imageFile/SQLiteHelper.java
+++ app/src/main/java/com/example/nhap/imageFile/SQLiteHelper.java
@@ -6,22 +6,48 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.database.sqlite.SQLiteStatement;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
 
 import androidx.annotation.Nullable;
 
+import com.example.nhap.R;
+
+import java.io.ByteArrayOutputStream;
 import java.util.ArrayList;
 import java.util.List;
 
 public class SQLiteHelper extends SQLiteOpenHelper {
-
+    Bitmap bitmap;
+    Context context;
     SQLiteDatabase db;
+    SQLiteStatement p;
     public SQLiteHelper(@Nullable Context context) {
-        super(context, "db", null, 4);
+        super(context, "db", null, 9);
+        this.context = context;
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
         db.execSQL("CREATE TABLE IF NOT EXISTS FOOD (ID INTEGER PRIMARY KEY AUTOINCREMENT, NAME VARCHAR, PRICE VARCHAR, IMAGE BLOB)");
+        p = db.compileStatement("insert into FOOD(NAME, PRICE, IMAGE) values(?, ?, ?)");
+
+        insertDataFood("a1", "834756834", R.drawable.a1);
+        insertDataFood("a2", "834756834", R.drawable.a2);
+        insertDataFood("a3", "834756834", R.drawable.a3);
+    }
+
+    public void insertDataFood(String name, String price, int img) {
+        bitmap = BitmapFactory.decodeResource(context.getResources(), img);
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.PNG, 0, stream);
+        byte[] data = stream.toByteArray();
+
+        p.bindString(1, name);
+        p.bindString(2, price);
+        p.bindBlob(3, data);
+        p.execute();
     }
 
     @Override
